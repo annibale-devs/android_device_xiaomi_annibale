fbf5fcb8fcdb81cb7463a2fc5a13fae4d06feff2
Arian <arian.kulmer@web.de>
2022-08-10 18:09:53 +0200

    annibale: Build fwk-detect from source
    Change-Id: I2915b0a9a10cade7c42df54cf3c7e48157d938e9

--- device.mk
+++ device.mk
@@ -169,6 +169,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/sun/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QTI
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti_vendor
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.class_main.sh \
