24e01c792f82d8f03dcf80a4c78044fa087437aa
Michael Bestas <mkbestas@gmail.com>
2022-11-07 17:49:42 +0200

    annibale: overlay: Enable multiple vibration strength levels support
    Change-Id: If17503f2dd4bd4d6e333891f9649a7db8d809852

--- device.mk
+++ device.mk
@@ -167,6 +167,7 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     SecureElementResTarget \
     SettingsResCommon \
+    SettingsResXiaomi \
     SystemUIResCommon \
     TelecommResCommon \
     TelephonyResCommon \
