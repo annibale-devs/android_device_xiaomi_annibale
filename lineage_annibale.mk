7aa1a9fc2a44fedf279dbb5afb575bdf93aa551a
ramshell688 <ramonzost68@gmail.com>
2025-11-29 22:14:31 +0700

    annibale: Initial skeleton tree
    Change-Id: Ibb116c01257e21979ccb36f174b0f951ab361fe6

---
+++ lineage_annibale.mk
@@ -0,0 +1,26 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from generic device
+$(call inherit-product, device/xiaomi/annibale/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_annibale
+PRODUCT_DEVICE := annibale
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 2510DPC44G
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="annibale-user 16 BP2A.250605.031.A3 OS3.0.6.0.WPKMIXM release-keys" \
+    BuildFingerprint := POCO/annibale_global/annibale:16/BP2A.250605.031.A3/OS3.0.6.0.WPKMIXM:user/release-keys
