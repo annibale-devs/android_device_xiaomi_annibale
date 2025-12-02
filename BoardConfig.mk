0832f6d8b2f17b02c97d8c8ef1d15df4caeff4bf
ramshell688 <ramonzost68@gmail.com>
2025-12-02 16:08:39 +0700

    annibale: Inherit common QTI compatibility matrix aidl
    Change-Id: I9ba54808ab9d6bd8819dc69117f60bbce5ca0555

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -220,6 +220,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/vintf/compatibility_matrix.device.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/vintf/manifest.xml
 
