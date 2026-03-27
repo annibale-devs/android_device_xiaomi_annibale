b782c3b1dc1b353245824b6c54280879f3114f5f
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-28 02:57:20 +0700

    annibale: Drop non existant labels
    Change-Id: I23d20d7b236618c46833dc83f680c3ce414afa1a

--- extract-files.py
+++ extract-files.py
@@ -233,9 +233,15 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.biometrics.fingerprint-V4-ndk.so'
         ),
     'odm/etc/init/vendor.xiaomi.hw.touchfeature-service.rc': blob_fixup()
-        .regex_replace(r'service touch-kmsg-init-sh\b[\s\S]*?\n(?=\S|$)', ''),
+        .regex_replace(r'service touch-kmsg-init-sh\b[\s\S]*?\n(?=\S|$)', '')
+        .regex_replace('seclabel.*?s0', ''),
     'vendor/etc/init/audiohalservice_qti.rc': blob_fixup()
         .regex_replace(r'service set_diag_state[\s\S]*?\n(?=\S|$)', ''),
+    (
+        'vendor/etc/init/hw/init.mi_thermald.rc',
+        'vendor/etc/init/hw/init.batterysecret.rc',
+    ): blob_fixup()
+        .regex_replace('seclabel.*?s0', ''),
     (
         'vendor/bin/wfdhdcphalservice',
         'vendor/bin/wfdvndservice'
