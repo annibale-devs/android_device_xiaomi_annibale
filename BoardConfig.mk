0c932f8ff4de9b5b220cc022dd71c05e35a04bc2
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-31 01:40:39 +0530

    annibale: sepolicy: Allow parts to use displayfeature
    Change-Id: I7b8a8ed50da73baebe1561df6dddd00ddc3f4044

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,6 +181,8 @@ VENDOR_SECURITY_PATCH := 2025-11-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
